f7bcb6a7ed24cefa9b7a642f2c626f44af56bcf5
userkw <elkis.kawthar11@gmail.com>
2024-06-02 12:58:22 -0700

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
 """
-Importing the flask module
+starts a Flask web application
 """
-from flask import Flask, render_template
-from models import storage
-from models.state import State
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def stateList():
-    """A function that displays the list of states from
-    the storage eather the fs or db."""
-    return render_template("7-states_list.html",
-                           statesStorage=storage.all(State))
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route("/states_list", strict_slashes=False)
-def teardown_appcontext():
-    """A function that removes the session after each reuest."""
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
